af908b14ee5b0fc981a786d3149f3e5b4fe7b58f
Steven Murr <stevemurr@gmail.com>
2017-08-27 16:06:16 -0700

    switches should be working.  requests are working

--- js/components/list/list-icon.js
+++ js/components/list/list-icon.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { Platform } from "react-native";
 import {
   Container,
@@ -28,13 +27,14 @@ class NHListIcon extends Component {
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.request = this.request.bind(this);
     this.state = {
       selectedItem: undefined,
       selected1: "key1",
       results: {
         items: []
       },
-      lights: true,
+      lights:false,
       waterPump: false,
       feedValve: false,
       mixValve: false,
@@ -45,13 +45,46 @@ class NHListIcon extends Component {
     };
   }
 
-  handleInputChange(event) {
-    console.log(event);
-    const target = event.target;
-    // const value = target.type === 'checkbox' ? target.checked : target.value;
-    const value = !target.value;
-    const name = target.name;
+  request(name) {
+    const deviceID = "3c0026000247353137323334";
+    const accessToken = "f235b0985b6b46d0b50e3ee93e051dfe1742b201";
+    const particleURL = "https://api.particle.io/v1/devices/" + deviceID + "/control";
 
+    var headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var params = {
+      "access_token": accessToken,
+      "args": name,
+    };
+
+    const searchParams = Object.keys(params).map((key) => {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    }).join('&');
+    
+    fetch(particleURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      body: searchParams
+    })
+
+    var settings = {
+      method: "POST",
+      body: searchParams,
+    };
+
+    fetch(particleURL, settings).then((res) => {
+      return res.json();
+    }).then((j) => {
+      // probably dont need to prin the json
+      console.log(j)
+    })
+  }
+  // handleInputChange is called when the switch is toggled
+  handleInputChange(value, name) {
+    this.request(name);
     this.setState({
       [name]: value
     });
@@ -90,7 +123,7 @@ class NHListIcon extends Component {
               <Text>Lights</Text>
             </Body>
             <Right>
-              <Switch onValueChange={(value) => this.setState({lights: value})} name="lights" value={this.state.lights} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "lights")} value={this.state.lights} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -104,7 +137,7 @@ class NHListIcon extends Component {
               <Text>Water Pump</Text>
             </Body>
             <Right>
-              <Switch name="waterPump" value={this.state.waterPump} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "waterPump")} value={this.state.waterPump} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -118,7 +151,7 @@ class NHListIcon extends Component {
               <Text>Feed Valve</Text>
             </Body>
             <Right>
-              <Switch name="feedValve" value={this.state.feedValve} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "feedValve")} value={this.state.feedValve} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -132,7 +165,7 @@ class NHListIcon extends Component {
               <Text>Mix Valve</Text>
             </Body>
             <Right>
-              <Switch name="mixValve" value={this.state.mixValve} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "mixValve")} value={this.state.mixValve} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -146,7 +179,7 @@ class NHListIcon extends Component {
               <Text>Drain Valve</Text>
             </Body>
             <Right>
-              <Switch name="drainValve" value={this.state.drainValve} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "drainValve")} value={this.state.drainValve} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -161,7 +194,7 @@ class NHListIcon extends Component {
               <Text>Water Fill</Text>
             </Body>
             <Right>
-              <Switch name="waterFill" value={this.state.waterFill} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "waterFill")} value={this.state.waterFill} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -175,7 +208,7 @@ class NHListIcon extends Component {
               <Text>HVAC Damper</Text>
             </Body>
             <Right>
-              <Switch name="hvacDamper" value={this.state.hvacDamper} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "hvacDamper")} value={this.state.hvacDamper} onTintColor="#50B948" />
             </Right>
           </ListItem>
 
@@ -189,7 +222,7 @@ class NHListIcon extends Component {
               <Text>CO2</Text>
             </Body>
             <Right>
-              <Switch name="co2" value={false} onTintColor="#50B948" />
+              <Switch onValueChange={(value) => this.handleInputChange(value, "co2")} value={false} onTintColor="#50B948" />
             </Right>
           </ListItem>
         </Content>
